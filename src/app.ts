07612b426f9271e713018abf15902cb5992a7a2d
IamRKG <radhacreative@gmail.com>
2026-08-10 08:40:44 -0500

    feat: add environment validation, database setup, and products API

--- src/app.ts
+++ src/app.ts
@@ -1,9 +1,19 @@
 import express, { type Express, type Request, type Response } from 'express';
+import { productsTable } from './db/schema.ts';
+import { db } from './index.ts';
 
 const app: Express = express();
+app.use(express.json());
 
 app.get('/', (req: Request, res: Response) => {
   res.send('Hello World!');
+});
+
+app.post('/products', async (req: Request, res: Response) => {
+  const { name, price, description } = req.body;
+  await db.insert(productsTable).values({ name, price, description });
+
+  res.send('Product created!');
 });
 
 app.listen(6000);
